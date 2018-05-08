e94b932d7c7b28be041294ef3e7c8628e1d2fbbf
Simon Michalke <simon.m.m@freenet.de>
2018-05-08 17:54:08 +0200

    implemented fetching settings data from vanilla server directory

--- fetch_settings.py
+++ fetch_settings.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
 import json
+import sys
+from shutil import copyfile
 
+if len(sys.argv) != 3:
+  print('please specify a game key and a folder')
+  exit(1)
 
+game = sys.argv[1]
+folder = sys.argv[2]
+
+config_file_data = ''
+
+with open(game+'/data.json', 'r') as config_file:
+  all_config_file_data = json.loads(config_file.read())
+
+for file_data in all_config_file_data:
+  print('copying "' + folder + '/' + file_data['location'] + '" to "' + game + '/' + file_data['file'] + '"')
+  copyfile(folder + '/' + file_data['location'],  game + '/' + file_data['file'])
